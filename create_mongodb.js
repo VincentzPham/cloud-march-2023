5d747ef5525ce4c7aa23d9b4f3393fbe719d55b7
B0nusduck <88030859+B0nusduck@users.noreply.github.com>
2023-03-04 23:52:43 +0700

    Delete create_mongodb.js

--- create_mongodb.js
+++
@@ -1,9 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://127.0.0.1:27017/mydb";
-
-MongoClient.connect(url, function(err,db){
-    if(err) throw err;
-    console.log("data base created!");
-    db.close();
-});
-// require "mpn install mongodb@4.14.0" to work
